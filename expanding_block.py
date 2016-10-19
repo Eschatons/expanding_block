dd378e459ecc0eb0e995b24e92d9c46ae957a8d5
Efron <Efron_Licht@yahoo.com>
2016-10-19 13:22:54 -0700

    refactoring of comments, code structure

--- expanding_block.py
+++ expanding_block.py
@@ -16,8 +16,6 @@ from mask import create_mask, write_mask
 from process_bucket import process_bucket
 from itertools import dropwhile
 
-# helper functions start here
-
         
 def expanding_block(filename, *, _debug = False):
     """
@@ -43,9 +41,43 @@ output:
     if imageConsideredModified == False,    imgOut is a (2m+8) x n x 3 image
     """
 
+    """ 
+    helper functions
     """
-    0. file IO and conversion to grayscale:
+
+    def _generate_groups(blocks):
+        """ assign blocks within image evenly to groups"""
+        group = []
+        blocksPerGroup = len(blocks) / init.numBuckets
+        group = 0
+        count = 0
+
+        for block in blocks:
+            if len(group) >= blocksPerGroup:
+                yield group
+                count -= blocksPerGroup
+                group = []
+            group.append(block)
+            count += 1
+        if len(group) > 0:
+            yield group
+            
+    def _generate_buckets(groups):
+        """ assign blocks within groups to overlapping buckets """
+        for n in range(len(groups)):
+            try:
+                bucket = groups[n-1] + groups[n] + groups[n+1]
+            except IndexError:
+                if n == init.numBuckets-1:
+                    bucket = groups[n-1]+groups[n]
+                else:
+                    raise
+            yield bucket
+    
     """
+    MAIN FUNCTION START
+    """
+    #0. file IO and conversion to grayscale:
 
     baseImg = io.imread(filename)
 
@@ -74,88 +106,38 @@ output:
         )
     blocks = sorted(blocks, key = lambda block: block.variance)
 
-    """
-    1. remove elements with too low of variance to cut down on false positives
-    due to bad white balance on source camera or areas of block color
-    """
+    #1. remove elements with too low of variance to cut down on false positives
+    #    due to bad white balance on source camera or areas of block color
     
     blocks = dropwhile(lambda block: block.tooLowVariance, blocks)
-
-    """
-    2. assign blocks as evenly as possible to groups
-    """
-    def _generate_groups(blocks):
-        group = []
-        # we don't want to use * to avoid nasty by-reference bugs
-        blocksPerGroup = len(blocks) / init.numBuckets
-        group = 0
-        count = 0
-
-        for block in blocks:
-            if len(group) >= blocksPerGroup:
-                yield group
-                count -= blocksPerGroup
-                group = []
-            group.append(block)
-            count += 1
-        # flush cache of blocks
-        if count > 0:
-            yield group
+    
+    #2. assign blocks as evenly as possible to groups
     groups = _generate_groups(blocks)
-
-    """
-    3. assign blocks in neighboring groups to overlapping buckets
-    """
-    def _generate_buckets(groups):
-        for n in range(len(groups)):
-            try:
-                bucket = groups[n-1] + groups[n] + groups[n+1]
-            except IndexError:
-                if n == init.numBuckets-1:
-                    bucket = groups[n-1]+groups[n]
-                else:
-                    raise
-            yield bucket
+    
+    #3. assign blocks in neighboring groups to overlapping buckets
     buckets = _generate_buckets(groups)
-    """
-    4. process buckets for pixel-to-pixel similiarity. see process_bucket
-    """    
+    
+    #4. process buckets for pixel-to-pixel similiarity. see process_bucket
     buckets = (process_bucket(bucket) for bucket in buckets)
     
     
     #buckets now hold blocks that we think are modified.
-    """
-    5. recombine buckets after processing
-    """
+    #5. recombine buckets after processing
     blocks = [block for bucket in buckets for block in bucket]
-        
-
-    #if there are no blocks left we consider image 'clean'
-    
-
+    #6a. Image is considered clean if no blocks left after processing
     if len(blocks) == 0:
-        """
-        6a. If image is considred clean, end of processing step.
-        """
-        imageConsideredModified = False
-        imgOut = baseImg
-    #else
 
+
+        imageConsideredModified = False
+        imgOut = baseImg    
+    #6b. If image is considered modified,create mask for image
     else:
         imageConsideredModified = True
-        """
-        6b. If image is considered modified,create mask for image
-        """
+
         mask = create_mask(blocks, baseImg, init)
-    
-        """
-        7b. Paint mask over image
-        """ 
+    #7b. Paint mask over image
         imgOut = np.uint8(write_mask(mask, baseImg))
-    
-    """
-    8. Output.
-    """
+    #8. Output.
     if _debug:
         io.imshow(imgOut)
     
